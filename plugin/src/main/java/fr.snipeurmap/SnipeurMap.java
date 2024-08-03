7cf5d8515a7fded682d55036fa65d225d52bdecc
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-03 23:42:55 +0200

    Create SnipeurMap.java

---
+++ plugin/src/main/java/fr.snipeurmap/SnipeurMap.java
@@ -0,0 +1,39 @@
+package fr.snipeurmap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public final class SnipeurMap extends JavaPlugin {
+    private Timer timerSendRequest;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Plugin SnipeurMap démarré !");
+
+
+        //on enregistre un timer
+        timerSendRequest = new Timer();
+
+        // tick delay
+        long delay = 3000;
+
+
+        // enregistrer la tâche à exécuter
+        timerSendRequest.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //la fonction à exécuter
+            }
+        }, 0, delay);
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
