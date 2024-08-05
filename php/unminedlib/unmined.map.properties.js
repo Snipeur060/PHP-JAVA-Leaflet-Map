bacb6eb21a6ffdeb40b75b9646275bb040e17109
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-05 11:29:03 +0200

    Create  unmined.map.properties.js

---
+++ php/unminedlib/unmined.map.properties.js
@@ -0,0 +1,11 @@
+var UnminedMapProperties = {
+    minZoom: -2,
+    maxZoom: 2,
+    defaultZoom: 0,
+    imageFormat: "png",
+    minRegionX: -2,
+    minRegionZ: -2,
+    maxRegionX: 1,
+    maxRegionZ: 2,
+    worldName: "world"
+}
