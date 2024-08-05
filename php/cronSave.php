b386e77bf1e969f178b521e4df43ef2132e4e969
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-05 11:28:06 +0200

    Create cronSave.php

---
+++ php/cronSave.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Sql DATABASE : (id,nom, x, y, z, heure,uuid)
+id : int Auto Increment
+nom: VARCHAR 255
+x: VARCHAR 255
+y: VARCHAR 255
+z: VARCHAR 255
+heure: VARCHAR 255
+uuid: VARCHAR 255
+*/
+/*if (in_array($_SERVER['REMOTE_ADDR'],$autorizedip) != true) {
+	echo "Fatal Error: Ip not whitelist for this";
+	die();
+}*/
+
+// Effectuer une requête vers l'API
+$url = "https://web.domain.fr/api/v1/showpos";
+$response = file_get_contents($url);
+// Vérifier si la requête a réussi
+if ($response !== false and $response != "[]") {
+    // Convertir les données JSON en un tableau associatif PHP
+    $data = json_decode($response, true);
+
+    // Préparer les valeurs pour la requête SQL
+    $values = array();
+    foreach ($data as $playerData) {
+        $nom = $playerData['nom'];
+        $x = floatval($playerData['x']);
+        $y = floatval($playerData['y']);
+        $z = floatval($playerData['z']);
+		$uuid =  $playerData['uuid'];
+        $heure = date("Y-m-d H:i:s"); // Obtenir l'heure actuelle
+
+        // Ajouter les valeurs à la liste des valeurs pour la requête SQL
+        $values[] = "('$nom', $x, $y, $z, '$heure','$uuid')";
+    }
+
+    // Construire la requête SQL
+    $sql = "INSERT INTO playersposlog (nom, x, y, z, heure,uuid) VALUES " . implode(",", $values);
+
+    // Exécuter la requête SQL
+    if ($db->query($sql) === TRUE) {
+        echo "Données insérées avec succès.\n";
+    } else {
+        echo "Erreur lors de l'insertion des données : " . $db->error . "\n";
+    }
+
+} else {
+    echo "Échec de la requête vers l'API.";
+}
+?>
