8ccea01e2d4203000ea140018bef90f7c47daad3
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-05 11:31:03 +0200

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta tags  -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>ENPA - Dashboard</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+    <!-- Leaflet -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.js"
+        crossorigin=""></script>
+	  
+	  <script src="
+https://cdn.jsdelivr.net/npm/leaflet-rotate-map@0.3.1/leaflet-src.min.js
+"></script>
+<link href="
+https://cdn.jsdelivr.net/npm/leaflet-rotate-map@0.3.1/leaflet.min.css
+" rel="stylesheet">
+
+    <!-- Leaflet tools -->
+    <script type="text/javascript" src="unminedlib/NoGap.js"></script>
+    <script type="text/javascript" src="unminedlib/functionaltilelayer.js"></script>
+
+    <!-- Map info -->
+    <script type="text/javascript" src="unminedlib/unmined.map.properties.js"></script>
+    <script type="text/javascript" src="unminedlib/unmined.map.regions.js"></script>
+
+    <!-- uNmINeD -->
+    <script type="text/javascript" src="unminedlib/unmined.leaflet.js"></script>
+
+  </head>
+
+</html>
+
